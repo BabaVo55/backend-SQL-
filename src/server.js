dea0ae8ddaff3da12d1c9aba2fd32b3b083d4acd
BabaVo55 <ethemcode@outlook.com>
2025-03-24 16:14:15 +0000

    app.use(json()) added

--- src/server.js
+++ src/server.js
@@ -15,6 +15,8 @@ const __dirname = dirname(__filename)
 // Serves the HTML file from the /public directory
 // Tells express to serve all file from the public folder as static assets/ file.
 
+app.use(express.static(path.join(__dirname, "../public")))
+
 // Serving up the HTML file from th  e /public directory
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
