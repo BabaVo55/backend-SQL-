40c05266fc69d4baa15e03a1ceff7a7259de7cd1
BabaVo55 <ethemcode@outlook.com>
2025-03-26 14:45:49 +0000

    huh?

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import db from '../db';
+const router = express.Router();
+
+
+
+
+router.post('/register', (req, res) => {
+
+})
+
+
+router.post('/login', (req,res) => {
+
+})
+
+
+export default router
