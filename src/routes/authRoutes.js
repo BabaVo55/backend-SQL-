20a94dc347fce029932611f8804daff4816c2148
BabaVo55 <ethemcode@outlook.com>
2025-03-27 17:33:54 +0000

    back at it again

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -7,13 +7,26 @@ import db from '../db.js';
 const router = express.Router();
 
 router.post('/register',(req, res) => {
-    const {username, password} = req.body;
+    const {username, password} = req.body; 
+    const hashedPassword = bcrypt.hashSync(password, 8);
+    console.log(hashedPassword)
     console.log(username, password)
-    res.status(202)
     res.send(`
         <h1>${username}</h1>
         <p>${password}</p>
+        <p>${hashedPassword}</p>
         `)
+    // save new user and password to database   
+    try {
+
+    } catch(error){
+        console.log(error.message);
+        res.sendStatus(500);
+    }   
+        
+        
+        
+    res.status(202)
 })
 
 
